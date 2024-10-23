dd523216c489b0228b1e19d3245341c1de7a908d
Shubh <Shubhftw@gmail.com>
2024-10-23 19:52:20 -0400

    Updated video cards

--- nova-ui/src/app/components/ColorModeToggle.tsx
+++ nova-ui/src/app/components/ColorModeToggle.tsx
@@ -1,42 +1,42 @@
 // src/app/components/ColorModeToggle.tsx
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const ColorModeToggle = () => {
-    const [darkMode, setDarkMode] = useState(false); // Default to light mode
+  const [darkMode, setDarkMode] = useState(false); // Default to light mode
 
-    useEffect(() => {
-        const currentTheme = window.localStorage.getItem('theme'); // Check theme from local storage
-        if (currentTheme === 'dark') {
-            setDarkMode(true);
-            document.documentElement.classList.add('dark');
-        } else {
-            setDarkMode(false); // Set light mode as the default if no preference is found
-            document.documentElement.classList.remove('dark');
-        }
-    }, []);
+  useEffect(() => {
+    const currentTheme = window.localStorage.getItem("theme"); // Check theme from local storage
+    if (currentTheme === "dark") {
+      setDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      setDarkMode(false); // Set light mode as the default if no preference is found
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
 
-    const toggleDarkMode = () => {
-        const newDarkMode = !darkMode;
-        setDarkMode(newDarkMode);
-        document.documentElement.classList.toggle('dark', newDarkMode);
-        window.localStorage.setItem('theme', newDarkMode ? 'dark' : 'light');
-    };
+  const toggleDarkMode = () => {
+    const newDarkMode = !darkMode;
+    setDarkMode(newDarkMode);
+    document.documentElement.classList.toggle("dark", newDarkMode);
+    window.localStorage.setItem("theme", newDarkMode ? "dark" : "light");
+  };
 
-    return (
-        <div className="color-mode-toggle-container">
-            <span className="color-mode-label">Dark Mode</span>
-            <div className="switch">
-                <input
-                    id="dark-mode-switch"
-                    type="checkbox"
-                    checked={darkMode}
-                    onChange={toggleDarkMode}
-                    className="color-mode-input"
-                />
-                <span className="slider round"></span>
-            </div>
-        </div>
-    );
+  return (
+    <div className="color-mode-toggle-container">
+      <span className="color-mode-label">Dark Mode</span>
+      <div className="switch">
+        <input
+          id="dark-mode-switch"
+          type="checkbox"
+          checked={darkMode}
+          onChange={toggleDarkMode}
+          className="color-mode-input"
+        />
+        <span className="slider round"></span>
+      </div>
+    </div>
+  );
 };
 
 export default ColorModeToggle;
