6b09b730c51c74c91bfde3b94c104a45cec6ec8a
notWizzy <kashyap76315@outlook.com>
2024-11-03 23:43:43 -0500

    work in progress2

--- nova-ui/next.config.mjs
+++ nova-ui/next.config.mjs
@@ -1,11 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
-
-
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
